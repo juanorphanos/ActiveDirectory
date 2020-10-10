5c97feac21d81fe789749d920d04d922dabeb373
Juan Pablo Orphanos <18380875+juanorphanos@users.noreply.github.com>
2020-10-10 13:46:14 -0300

    Create Add-Attribute.ps1

---
+++ AddAttributeScheduledTask/Add-Attribute.ps1
@@ -0,0 +1,26 @@
+###############################################################################################################
+# Script - Populate Fields         Author:Juan Pablo Orphanos                                                 #
+# Description: This scripts is meant to be triggered by a User Creation Event (event 4720 in Event Log)       #
+# (evento 4720 en el Security Event log)                                                                      #
+# The script autopopulates some Active Directory the Company Attribute                                        #
+#                                                                                                             #
+#                                                                                                             #
+###############################################################################################################
+
+param ($username)
+
+$date = Get-Date -Format ddMMyyyy-hhmmsss
+$day = Get-Date -Format ddMMyyyy
+$logFile = "C:\AutoPopulateCompanyScript\log-$day.log"
+$company = 'Contoso'
+
+$aduser = Get-ADUser -properties Description -Identity $username
+   try{
+   Set-ADUser -Identity $aduser -Company $company
+            $changemessage = "$fecha : User $username, modified, attribute Company set in $company"
+            Out-File -FilePath $logFile -Append -InputObject $changemessage
+         }
+        catch{
+            $failedMessage = "$fecha : Error, could not set attribute Company as $company for user $username"
+            Out-File -FilePath $logFile -Append -InputObject $failedmessage
+        }
